00b49b5a9f6383713bd9abde60a7fcd3dd37b627
Rok Koren <rokoren@gmail.com>
2025-06-29 23:23:29 +0200

    Spring Boot Initializr

---
+++ src/test/java/rokoren/taxation/TaxationApplicationTests.java
@@ -0,0 +1,13 @@
+package rokoren.taxation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
